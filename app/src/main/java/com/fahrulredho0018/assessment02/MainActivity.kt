76b3b6005c5d5edca767812e57bfead708bf896c
Fahrul redho <faruledo@gmail.com>
2025-05-04 15:08:28 +0700

    Menangani keadaan jika list kosong

--- app/src/main/java/com/fahrulredho0018/assessment02/MainActivity.kt
+++ app/src/main/java/com/fahrulredho0018/assessment02/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -66,15 +67,25 @@ fun MainScreen() {
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
     val viewModel: MainViewModel = viewModel()
-    val data = viewModel.data
+    val data = emptyList<Penulis>()
 
-
-    LazyColumn(
-        modifier = modifier.fillMaxSize()
-    ) {
-        items(data) {
-            ListItem(penulis = it)
-            HorizontalDivider()
+    if (data.isEmpty()) {
+        Column(
+            modifier = modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = stringResource(id = R.string.list_kosong))
+        }
+    }
+    else {
+        LazyColumn(
+            modifier = modifier.fillMaxSize()
+        ) {
+            items(data) {
+                ListItem(penulis = it)
+                HorizontalDivider()
+            }
         }
     }
 }
