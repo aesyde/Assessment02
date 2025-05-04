a8452fcdece46cbc88e42205706b1fbb60ebfc7c
Fahrul redho <faruledo@gmail.com>
2025-05-04 15:04:27 +0700

    Membuat tampilan list

--- app/src/main/java/com/fahrulredho0018/assessment02/MainActivity.kt
+++ app/src/main/java/com/fahrulredho0018/assessment02/MainActivity.kt
@@ -5,8 +5,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,7 +22,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.fahrulredho0018.assessment02.model.Penulis
+import com.fahrulredho0018.assessment02.screen.MainViewModel
 import com.fahrulredho0018.assessment02.ui.theme.Assessment02Theme
 
 class MainActivity : ComponentActivity() {
@@ -52,10 +65,40 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Halooo",
-        modifier = modifier
-    )
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
+
+
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
+    ) {
+        items(data) {
+            ListItem(penulis = it)
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun ListItem(penulis: Penulis) {
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(text = penulis.judul,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = penulis.namapenulis,
+                maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(text = penulis.tanggal,
+                maxLines = 3,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/fahrulredho0018/assessment02/screen/MainViewModel.kt
+++ app/src/main/java/com/fahrulredho0018/assessment02/screen/MainViewModel.kt
@@ -8,12 +8,42 @@ class MainViewModel : ViewModel() {
     val data = listOf(
         Penulis(
             1,
-            "Kuliah Mobpro 17 Feb",
-            "Kuliah hari pertamaaa kuliahhhhhhhhhh",
+            "walah we",
+            "Suci",
             "2025-02-17 12:34:56"
         ),
         Penulis(
             2,
+            "aku adalah ironmen",
+            "Daffa",
+            "2025-02-19 11:22:50"
+        ),
+        Penulis(
+            3,
+            "aku adalah ironmen",
+            "Daffa",
+            "2025-02-19 11:22:50"
+        ),
+        Penulis(
+            4,
+            "aku adalah ironmen",
+            "Daffa",
+            "2025-02-19 11:22:50"
+        ),
+        Penulis(
+            5,
+            "aku adalah ironmen",
+            "Daffa",
+            "2025-02-19 11:22:50"
+        ),
+        Penulis(
+            6,
+            "aku adalah ironmen",
+            "Daffa",
+            "2025-02-19 11:22:50"
+        ),
+        Penulis(
+            7,
             "aku adalah ironmen",
             "Daffa",
             "2025-02-19 11:22:50"
